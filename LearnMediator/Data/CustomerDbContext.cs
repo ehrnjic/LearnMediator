2ac20a4c0b78a618dc13beff149b2490b2b830e3
ehrnjic <ehrnjic@gmail.com>
2022-04-07 05:51:22 +0200

    Resolve uninitialized DbSet warning (non-nullable)

--- LearnMediator/Data/CustomerDbContext.cs
+++ LearnMediator/Data/CustomerDbContext.cs
@@ -1,12 +1,19 @@
 ﻿using LearnMediator.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace LearnMediator.Data
+namespace LearnMediator.Data;
+
+public class CustomerDbContext : DbContext
 {
-    public class CustomerDbContext : DbContext
-    {
-        public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options) { }
-        public DbSet<Customer> Customers { get; set; }
-      
-    }
+    public CustomerDbContext(DbContextOptions<CustomerDbContext> options) : base(options) { }
+    public DbSet<Customer> Customers { get; set; } = null!;
+
+    /* The common practice of having uninitialized DbSet properties on context types is also
+     * problematic, as the compiler will now emit warnings for them. This can be fixed as 
+     * follows: public DbSet<Customer> Customers => Set<Customer>();
+     * Another strategy is to use non-nullable auto-properties, but to initialize them to null,
+     * using the null-forgiving operator (!) to silence the compiler warning. The DbContext base 
+     * constructor ensures that all DbSet properties will get initialized, and null will never 
+     * be observed on them.
+     */
 }
